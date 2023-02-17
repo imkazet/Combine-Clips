e03106a5d7a64a643f5da602d20a3cafac8e68ab
imkazet <125702415+imkazet@users.noreply.github.com>
2023-02-17 22:48:03 +0000

    Update concatclips.py

--- concatclips.py
+++ concatclips.py
@@ -9,7 +9,7 @@ def main():
     for count, file_name in enumerate(os.listdir(dir_path)):
         #Checks for video files with .mkv extension (might not work with other formats though there is no reason it shouldn't)
         if file_name.endswith(".mkv"):
-            dst = f"clip{str(count)}.mkv"
+            dst = f"video{str(count)}.mkv"
             src =f"{dir_path}/{file_name}"
             dst =f"{dir_path}/{dst}"
             
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 # Open the output file in write mode
 with open("output.txt", "w") as output_file:
     # Iterate over all files in the directory
-    for file_name in os.listdir(dir_path):
+    for file_name in sorted(os.listdir(dir_path)):
         # Check if the file has a .mkv extension
         if file_name.endswith(".mkv"):
             # Write the file name to the output file
